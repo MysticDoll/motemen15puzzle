243555e4b134ac2f2b533ba613ce172a4f3b733a
Mitsuru Takigahira <mysticdoll@hatena.ne.jp>
2023-07-12 11:27:19 +0900

    fix randomize function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Board from './Board';
-import {motemen} from './motemen';
+import {white, motemen} from './motemen';
+
+const randomize = (motemen) => {
+  let m = motemen.slice();
+  Array(Math.floor(Math.random() * 10000)).fill().map(() => 
+    [-1, 1, 4, -4][Math.floor(Math.random() * 10000) % 4]
+  ).forEach(i => {
+    const w = m.findIndex(c => c === white);
+    if((w + i) >= 0 && (w + i) < 16) {
+      let wc = m[w];
+      m[w] = m[w + i];
+      m[w + i] = wc;
+    }
+  });
+
+  return m;
+}
 
 function App() {
-  const motemenRandomized = motemen.slice().sort(() => Math.random() > 0.5 ? 1 : -1);
+  const motemenRandomized = randomize(motemen);
 
   return (
     <div className="App">
